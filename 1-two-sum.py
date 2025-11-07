c1336893991755f127458be3276a64893541ee66
gmartineza <68785609+gmartineza@users.noreply.github.com>
2025-11-07 03:01:07 -0300

    update two sum w/better solution

--- 1-two-sum.py
+++ 1-two-sum.py
@@ -1,10 +1,22 @@
+"""
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         for i in range(len(nums)):
-            remainder = target - nums[i]
+            complement = target - nums[i]
             for j in range(len(nums)):
                 if j == i:
                     pass
-                elif nums[j] == remainder:
+                elif nums[j] == complement:
                     return [i, j]
+"""
 
+class Solution: #(better)
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap = {}
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in hashmap and hashmap[complement] != i:
+                return [hashmap[complement], i]
+            else:
+                hashmap[nums[i]] = i
+        return []
