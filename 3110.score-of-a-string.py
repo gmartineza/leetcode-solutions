eb87870c23b2ee4ec4ff2b644c24f93e17bca662
gmartineza <68785609+gmartineza@users.noreply.github.com>
2025-11-08 15:25:24 -0300

    score of a string

---
+++ 3110.score-of-a-string.py
@@ -0,0 +1,48 @@
+# @leet imports start
+from string import *
+from re import *
+from datetime import *
+from collections import *
+from heapq import *
+from bisect import *
+from copy import *
+from math import *
+from random import *
+from statistics import *
+from itertools import *
+from functools import *
+from operator import *
+from io import *
+from sys import *
+from json import *
+from builtins import *
+import string
+import re
+import datetime
+import collections
+import heapq
+import bisect
+import copy
+import math
+import random
+import statistics
+import itertools
+import functools
+import operator
+import io
+import sys
+import json
+from typing import *
+# @leet imports end
+
+# @leet start
+class Solution:
+    def scoreOfString(self, s: str) -> int:
+        chars = list(s)
+        print(chars)
+        result = 0
+        for i in range(1, len(chars)):
+            result += abs(ord(chars[i]) - ord(chars[i-1]))
+        return result
+
+# @leet end
